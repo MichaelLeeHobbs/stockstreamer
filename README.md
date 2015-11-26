b947bf43271e3f7f0440e6367bcf4674a7c1ca14
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-11-26 10:02:24 -0500

    task 5.4 done

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ This project was generated with the [Angular Full-Stack Generator](https://githu
   1. Need a graph using angularcharts.js - Note: consider D3.js due to hide/show bug with chart.js which angularchart.js uses
   2. ~~Need a nav list with a self remove button for each item - using generated things api~~
   3. ~~Need an add new input and button - using generated things api~~
-  4. Update or remove the tool tip from existing things list
+  4. ~~Update or remove the tool tip from existing things list~~
   5. ~~Enable adding/removing stocks in real time~~
   6. ~~Prevent duplicate entries and ignore case~~
   
--- server/config/seed.js
+++ server/config/seed.js
@@ -9,32 +9,17 @@ import Thing from '../api/thing/thing.model';
 Thing.find({}).removeAsync()
   .then(function() {
     Thing.create({
-      name: 'Development Tools',
-      info: 'Integration with popular tools such as Bower, Grunt, Babel, Karma, ' +
-             'Mocha, JSHint, Node Inspector, Livereload, Protractor, Jade, ' +
-             'Stylus, Sass, and Less.'
+      name: 'FB'
     }, {
-      name: 'Server and Client integration',
-      info: 'Built with a powerful and fun stack: MongoDB, Express, ' +
-             'AngularJS, and Node.'
+      name: 'XOM'
     }, {
-      name: 'Smart Build System',
-      info: 'Build system ignores `spec` files, allowing you to keep ' +
-             'tests alongside code. Automatic injection of scripts and ' +
-             'styles into your index.html'
+      name: 'VZ'
     }, {
-      name: 'Modular Structure',
-      info: 'Best practice client and server structures allow for more ' +
-             'code reusability and maximum scalability'
+      name: 'AAPL'
     }, {
-      name: 'Optimized Build',
-      info: 'Build process packs up your templates as a single JavaScript ' +
-             'payload, minifies your scripts/css/images, and rewrites asset ' +
-             'names for caching.'
+      name: 'GOOG'
     }, {
-      name: 'Deployment Ready',
-      info: 'Easily deploy your app to Heroku or Openshift with the heroku ' +
-             'and openshift subgenerators'
+      name: 'AMZN'
     });
   });
 
