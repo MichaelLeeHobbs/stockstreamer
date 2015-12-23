0468b3d4787282a9406248a1b801c818423f98ca
Michael Hobbs <michael.lee.hobbs@gmail.com>
2015-12-23 07:42:19 -0500

    formatting and cleanup

--- client/app/stockGetter/stockGetter.service.js
+++ client/app/stockGetter/stockGetter.service.js
@@ -5,27 +5,19 @@ angular.module('publicHtmlApp')
     // AngularJS will instantiate a singleton by calling "new" on this function
 
     return {
-      get: function (stockSymbols, callBack) {
-        console.log(stockSymbols);
-        if (stockSymbols.length === 0) {
-          console.log('stockSymbols.length: ' + stockSymbols.length);
-          //return;
-        }
+      get: function (stockSymbol, callBack) {
 
         var startDate = new Date();
         startDate.setYear(startDate.getUTCFullYear() - 1);
         startDate = (startDate.getUTCFullYear()) + '-' + (startDate.getUTCMonth()) + '-' + (startDate.getUTCDate());
 
-        stockSymbols.forEach(function (ele) {
-          console.log('getting ' + ele);
-          $http({
-            method: 'GET',
-            url:    'https://www.quandl.com/api/v3/datasets/WIKI/' + ele + '.json?start_date=' + startDate + '&api_key=ryj9aeja8R-59_bvByzo',
-            cache:  true
-          })
-            .success(function (data) {
-              callBack(ele, data);
-            });
+        $http({
+          method: 'GET',
+          url:    'https://www.quandl.com/api/v3/datasets/WIKI/' + stockSymbol + '.json?start_date=' + startDate + '&api_key=ryj9aeja8R-59_bvByzo',
+          cache:  true
+        })
+        .success(function (data) {
+          callBack(stockSymbol, data);
         });
       }
     };
